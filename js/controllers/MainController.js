ce71807f4b6efb232cd66fd40a180bdc4a3d8b76
Lucas Oliveira <lucas.f.pedrosa@gmail.com>
2019-03-25 00:19:47 -0300

    Implementando o hover sobre as imagens

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,18 +2,14 @@
 // manages the app's data. 
 app.controller('MainController', ['$scope', function($scope) { 
     $scope.images = [ { 
-        id: 1,
         cover: 'img/louvre-image.jpg',
         information: 'Tell me Louvre' },
     {
-        id: 2,
         cover: 'img/arco-do-triunfo.jpg',
         information: 'tudo sobre o Arco do Triunfo' },
     {
-        id: 3,
         cover: 'img/cathedral.jpg' },
     {
-        id: 4,
         cover: 'img/rio-sena.jpg' },
     ];
 }]);
